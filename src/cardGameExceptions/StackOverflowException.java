3698634f3c11e39fe25e87bb95450b9620dcc92d
Damilare Jolayemi <djolayemi@gmail.com>
2021-05-27 17:47:03 +0100

    added features

--- src/cardGameExceptions/StackOverflowException.java
+++ src/cardGameExceptions/StackOverflowException.java
@@ -3,6 +3,10 @@ package cardGameExceptions;
 public class StackOverflowException extends CardGameExceptions {
 
     public StackOverflowException(String message) {
+        super(message);
+    }
+    public StackOverflowException() {
         super();
     }
+
 }
