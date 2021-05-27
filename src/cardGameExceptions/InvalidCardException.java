2cec3446c577ae49a4bf216a910d696ee313c853
Damilare Jolayemi <djolayemi@gmail.com>
2021-05-27 17:47:38 +0100

    added features

--- src/cardGameExceptions/InvalidCardException.java
+++ src/cardGameExceptions/InvalidCardException.java
@@ -3,6 +3,9 @@ package cardGameExceptions;
 public class InvalidCardException extends CardGameExceptions{
 
     public InvalidCardException(String message) {
+        super(message);
+    }
+    public InvalidCardException() {
         super();
     }
 }
